cc1de2f95f32888808410a3d7c788553cadd4722
AntonBuketovEST <buketov.95@gmail.com>
2022-05-31 00:19:21 +0300

    fix query to db for zones

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', (req, res, next)=>{
+app.get('/', (req, res, next)=>{
   console.log("рендерим индекс")
   res.render("index")
 })
