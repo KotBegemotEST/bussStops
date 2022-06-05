036a09a3bd62b73f371e4409ba9d53cb7450ef84
AntonBuketovEST <buketov.95@gmail.com>
2022-06-05 13:21:19 +0300

    ready 1.1

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.get('/', (req, res)=>{
--- db/db_func.js
+++ db/db_func.js
@@ -1,7 +1,6 @@
 const mysql = require("mysql")
 var pool = require('mysqlconnector');
 const connection = mysql.createPool({
-
     host:"d26893.mysql.zonevs.eu",
     user:"d26893_busstops",
     password:"3w7PYquFJhver0!KdOfF",
@@ -24,7 +23,7 @@ async function readStops() {
 
 function getRegions() {
     return new Promise((resolve, reject) => {
-        const query = "SELECT stops.stop_area FROM antonBuketov_busStops GROUP BY stop_area;"
+        const query = "SELECT antonBuketov_busStops.stop_area FROM antonBuketov_busStops GROUP BY stop_area;"
         connection.query(query, (err, results) => {
             if (err) reject(new Error(err.message));
             console.log(results)
@@ -51,14 +50,17 @@ function getAllStops(stop_area){
 function getBuses(stop_area,stop_name){
     try {
         return new Promise((resolve, reject) => {
-        const query = "SELECT DISTINCT r.route_short_name\
-            FROM antonBuketov_busStops, antonBuketov_stopsTimes st, antonBuketov_trips t, antonBuketov_routes r\
-            WHERE st.stop_id = stops.stop_id AND \
-                  stops.stop_name = ? and \
-                  stops.stop_area = ? AND\
-                  t.trip_id = st.trip_id AND\
-                  r.route_id = t.route_id\
-            order by r.route_short_name";
+        const query = "SELECT antonBuketov_routes.route_short_name \
+        FROM (\
+            SELECT antonBuketov_busStops.stop_id, antonBuketov_busStops.stop_name, antonBuketov_stopsTimes.trip_id \
+            FROM antonBuketov_busStops \
+                Right join antonBuketov_stopsTimes on antonBuketov_stopsTimes.stop_id = antonBuketov_busStops.stop_id \
+            WHERE stop_name = ? and stop_area = ?\
+            GROUP BY antonBuketov_busStops.stop_id, antonBuketov_busStops.stop_name, antonBuketov_stopsTimes.trip_id \
+            ) tab_a \
+        LEFT JOIN antonBuketov_trips ON tab_a.trip_id = antonBuketov_trips.trip_id \
+        LEFT JOIN antonBuketov_routes ON antonBuketov_routes.route_id = antonBuketov_trips.route_id \
+        GROUP BY antonBuketov_routes.route_short_name ORDER BY antonBuketov_routes.route_short_name";
             connection.query(query, [stop_name, stop_area], (err, results) => {
                 if(err) reject(new Error(err.message));
                 resolve(results);
@@ -122,22 +124,36 @@ function getNearestStops(lat, lon){
 }
 
 
+// "SELECT st.departure_time, t.direction_code, t.trip_long_name,r.route_short_name,s.stop_name, 'today' as day \
+// FROM antonBuketov_busStops s, antonBuketov_stopsTimes st, antonBuketov_trips t, antonBuketov_routes r \
+// WHERE s.stop_id = st.stop_id AND \
+// st.trip_id = t.trip_id AND \
+// t.route_id = r.route_id AND \
+// s.stop_name = ? AND \
+// s.stop_area = ? AND \
+// st.departure_time > TIME(?) AND\
+// r.route_short_name = ?  \
+// GROUP BY st.departure_time LIMIT 5";
+
 
 function getTimes(stop_area, stop_name, route_short_name, dep_time) {
     let flag = false
     try{
         return new Promise((resolve, reject) => {
             const query =
-            "SELECT st.departure_time, t.direction_code, t.trip_long_name,r.route_short_name,s.stop_name, 'today' as day \
-            FROM antonBuketov_busStops s, antonBuketov_stopsTimes st, antonBuketov_trips t, antonBuketov_routes r \
-            WHERE s.stop_id = st.stop_id AND \
-            st.trip_id = t.trip_id AND \
-            t.route_id = r.route_id AND \
-            s.stop_name = ? AND \
-            s.stop_area = ? AND \
-            st.departure_time > TIME(?) AND\
-            r.route_short_name = ?  \
-            GROUP BY st.departure_time LIMIT 5";
+            "SELECT * FROM ( \
+                SELECT tab_b.* FROM ( \
+                SELECT tab_a.stop_id,tab_a.stop_area,tab_a.stop_name,tab_a.departure_time,tab_a.trip_id,antonBuketov_trips.trip_long_name,antonBuketov_routes.route_short_name \
+                FROM ( \
+                    SELECT antonBuketov_busStops.stop_area, antonBuketov_busStops.stop_id, antonBuketov_busStops.stop_name,antonBuketov_stopsTimes.trip_id, antonBuketov_stopsTimes.departure_time \
+                    FROM antonBuketov_busStops RIGHT JOIN antonBuketov_stopsTimes ON antonBuketov_stopsTimes.stop_id = antonBuketov_busStops.stop_id \
+                    WHERE stop_name = ? AND stop_area = ? AND antonBuketov_stopsTimes.departure_time >= TIME(?) \
+                    GROUP BY antonBuketov_busStops.stop_id, antonBuketov_busStops.stop_name, antonBuketov_stopsTimes.trip_id ) tab_a \
+                LEFT JOIN antonBuketov_trips ON tab_a.trip_id = antonBuketov_trips.trip_id \
+                LEFT JOIN antonBuketov_routes ON antonBuketov_routes.route_id = antonBuketov_trips.route_id \
+                WHERE antonBuketov_routes.route_short_name = ? \
+                ORDER BY tab_a.departure_time ) tab_b LIMIT 5) tab_c \
+                ORDER BY tab_c.departure_time" ;
 
             connection.query(query, [stop_name, stop_area,dep_time, route_short_name], (err, results) => {
                 if(err) reject(new Error(err.message));
